cd8627791246da00608d625d9720cf2a9f84085d
AnjaliRawke <anjalirawke21@gmail.com>
2023-03-25 22:23:48 +0530

    Added: Stack and Queue Problem

---
+++ src/com/bridgelabz/Stack/Node.java
@@ -0,0 +1,22 @@
+package com.bridgelabz.Stack;
+
+public class Node<T> {
+	T data;
+	Node<T> next;
+
+	public Node() {
+	}
+
+	public Node(T data){
+		this.setData(data);
+
+	}
+
+	public T getData() {
+		return data;
+	}
+
+	public void setData(T data) {
+		this.data = data;
+	}
+}
